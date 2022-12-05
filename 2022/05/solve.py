4007c47c95ee58c83165c685e9fc2dbeb93f1f60
pepperbob <github@byoc.de>
2022-12-05 21:35:38 +0100

    2022/05
    as good as it gets for the moment

--- 2022/05/solve.py
+++ 2022/05/solve.py
@@ -2,11 +2,16 @@ import re
 
 def transpose(matrix):
     """
-    Transpose Matrix 90° right
+    Transpose Matrix 90° (clockwise)
     """
     return [[matrix[j][i] for j in range(len(matrix)) if matrix[j][i] != ""] for i in range(len(matrix[0]))]
 
-def readInput(inputLines):
+def readInput():
+    """
+    Parse the input lines to stacks and operations, transpose stacks for easier handling
+    """
+    inputLines = open("input", "r").readlines()
+
     mode_op = False
     stacks = []
     operations = []
@@ -15,22 +20,20 @@ def readInput(inputLines):
         if line == "\n":
             mode_op = True
             continue
-
+        
         if mode_op == True:
-            operations.append(line)
+            operations.append(line.strip())
         else:
-            chunks = [line[i:i+n].strip() for i in range(0, len(line), n)]
+            chunks = [line[i:i+n].strip().replace("[", "").replace("]", "") for i in range(0, len(line), n)]
             stacks.append(chunks)
     return (transpose(stacks), operations)
 
-
 # Regex to parse commands
 command = r"move (\d+) from (\d+) to (\d+)"
-input = open("input", "r").readlines()
-
-(stacks, operations) = readInput(input)
 
 # part 1
+
+(stacks, operations) = readInput()
 for op in operations:
     (cnt, source, target) = re.match(command, op).groups()
     sourceStack = stacks[int(source)-1]
@@ -38,26 +41,25 @@ for op in operations:
     for i in range(int(cnt)):
         targetStack.insert(0, sourceStack.pop(0))
 
-cratesOnTopOfStack = "".join([i[0].replace("[", "").replace("]", "") for i in stacks])
+cratesOnTopOfStack = "".join([i[0] for i in stacks])
 
 print(f"Crates on top with Mover 9000: {cratesOnTopOfStack}")
+
 del stacks
+del operations
+del cratesOnTopOfStack
 
 # part 2
 (stacks, operations) = readInput(input)
-
 for op in operations:
     (cnt, source, target) = re.match(command, op).groups()
     sourceStack = stacks[int(source)-1]
     targetStack = stacks[int(target)-1]
-    stackToMove = []
+    cratesToMove = []
     for i in range(int(cnt)):
-        stackToMove.append(sourceStack.pop(0))
+        cratesToMove.append(sourceStack.pop(0))
+    
+    stacks[int(target)-1] = cratesToMove + targetStack
 
-
-    print(op.strip())
-    for i in reversed(stackToMove):
-        targetStack.insert(0, i)
-
-cratesOnTopOfStack = "".join([i[0].replace("[", "").replace("]", "") for i in stacks])
+cratesOnTopOfStack = "".join([i[0] for i in stacks])
 print(f"Crates on top with Mover 9001: {cratesOnTopOfStack}")
