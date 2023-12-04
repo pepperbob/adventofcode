7946c79315d4c6ac11472b54593e8f34dec246f9
pepperbob <github@byoc.de>
2023-12-04 22:35:32 +0100

    2023/04 - remove recursive; much faster

--- 2023/04/solve.py
+++ 2023/04/solve.py
@@ -1,43 +1,29 @@
 import math
+from collections import defaultdict
 
 rawinput = open("04/input").read().split("\n")
 
 input = []
 for r in rawinput:
     cards = r.split(":")[1]
-    wins, hand = cards.split("|")
-    wins = [x for x in wins.split(" ") if len(x) > 0]
-    hand = [x for x in hand.split(" ") if len(x) > 0]
+    wins, hand = map(str.split, cards.split("|"))
     input.append((wins, hand))
 
-
-res1 =  0
-game_wins = dict()
-
+res1 = 0
+games_played = defaultdict(lambda: 0)
 for g, c in enumerate(input):
-    game_wins[g] = []
-
+    games_played[g] += 1 # play current game
+    
     wins, hand = c
-    hand_win = [h for h in hand if h in wins]
-    winning_cards = len(hand_win)
-    if winning_cards > 0:
+    winning = len([h for h in hand if h in wins])
+    if winning > 0:
         # calculate fancy :see_no_evil:
-        res1 += int(math.pow(2, len(hand_win)-1))
-        # memorize stacks copied
-        game_wins[g] = [g+x for x in range(1, winning_cards+1)]  
+        res1 += int(math.pow(2, winning-1))
+        # part 2: additional next $winning games
+        for additional in range(g+1, g+winning+1):
+            games_played[additional] += games_played[g]
 
 print(f"1* Result: {res1}")
 
-# recursion, crazy slow
-def get_stack_size(game, win_map):
-    local_stack = 1
-    for x in win_map[game]:
-        local_stack += get_stack_size(x, win_map)
-    return local_stack
-
-# add stack size
-stack_size = 0
-for g in game_wins.keys():
-    stack_size += get_stack_size(g, game_wins)
-
+stack_size = sum(games_played.values())
 print(f"2* Result: {stack_size}")
