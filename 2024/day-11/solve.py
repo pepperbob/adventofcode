148446b1674c21c41b4f13495f5513d89e41e07c
pepperbob <github@byoc.de>
2024-12-11 08:26:22 +0100

    day-11 decorator for cache
    there is functools... but this is aoc - use the cache as decorator..

--- 2024/day-11/solve.py
+++ 2024/day-11/solve.py
@@ -1,12 +1,15 @@
 data = list(map(int, open("input").read().strip().split(" ")))
 
 cachemap = {}
-def cache(fn, arg1, arg2):
-    if (arg1, arg2) not in cachemap:
-        cachemap[(arg1, arg2)] = fn(arg1, arg2)
+def cache(fn):
+    def wrapped_fn(*arg, **kwargs):
+        if (arg) not in cachemap:
+            cachemap[arg] = fn(*arg, **kwargs)
+        return cachemap[arg]
 
-    return cachemap[(arg1, arg2)]
+    return wrapped_fn
 
+@cache
 def process_recursive(no, count):
     """ items are actually irrelevant, what counts is the item count
     so recursively calclate for every item and sum up the number of elements
@@ -15,16 +18,16 @@ def process_recursive(no, count):
         # we're done here
         return 1
     if no == 0:
-        return cache(process_recursive, 1, count-1)
+        return process_recursive(1, count-1)
     elif len(str(no))%2==0:
         stno = str(no)
         lstr = len(stno)//2
-        return cache(process_recursive,int(stno[0:lstr]), count-1) + process_recursive(int(stno[lstr:]), count-1)
+        return process_recursive(int(stno[0:lstr]), count-1) + process_recursive(int(stno[lstr:]), count-1)
     else:
-        return cache(process_recursive,no*2024, count-1)
+        return process_recursive(no*2024, count-1)
 
-sol1 = [cache(process_recursive, n, 25) for n in data]
+sol1 = [process_recursive(n, 25) for n in data]
 print("Solution 1", sum(sol1))
 
-sol2 = [cache(process_recursive, n, 75) for n in data]
+sol2 = [process_recursive(n, 75) for n in data]
 print("Solution 2", sum(sol2))
