ffc1e77e941a73b6c5d8a153d4e3cb9ee055c548
pepperbob <github@byoc.de>
2024-12-11 08:12:37 +0100

    day-11: handle exploding memory
    keeping a list with individual items is not required as only the total amount of items count
    
    so for every item calculate how many items will be created, use recursion and a simple cache lookup

--- 2024/day-11/solve.py
+++ 2024/day-11/solve.py
@@ -1,33 +1,30 @@
 data = list(map(int, open("input").read().strip().split(" ")))
 
-def split_in_2(no):
-    stno = str(no)
-    lstr = len(stno)//2
-    return list(map(int, (stno[0:lstr], stno[lstr:])))
+cachemap = {}
+def cache(fn, arg1, arg2):
+    if (arg1, arg2) not in cachemap:
+        cachemap[(arg1, arg2)] = fn(arg1, arg2)
 
-def multiply_with_2024(no):
-    return no*2024
+    return cachemap[(arg1, arg2)]
 
-def map_to_1(no):
-    return 1
+def process_recursive(no, count):
+    """ items are actually irrelevant, what counts is the item count
+    so recursively calclate for every item and sum up the number of elements
+    """
+    if count == 0:
+        # we're done here
+        return 1
+    if no == 0:
+        return cache(process_recursive, 1, count-1)
+    elif len(str(no))%2==0:
+        stno = str(no)
+        lstr = len(stno)//2
+        return cache(process_recursive,int(stno[0:lstr]), count-1) + process_recursive(int(stno[lstr:]), count-1)
+    else:
+        return cache(process_recursive,no*2024, count-1)
 
-def process(dat):
-    mapped = []
-    for i in dat:
-        if i == 0:
-            mapped.append(map_to_1(i))
-        elif len(str(i))%2 == 0:
-            r = split_in_2(i)
-            # print("Split", i, ":", r)
-            mapped.extend(r)
-        else:
-            mapped.append(multiply_with_2024(i))
-    return mapped
+sol1 = [cache(process_recursive, n, 25) for n in data]
+print("Solution 1", sum(sol1))
 
-
-round = 0
-while round < 25:
-    data = process(data)
-    round += 1
-
-print(len(data))
+sol2 = [cache(process_recursive, n, 75) for n in data]
+print("Solution 2", sum(sol2))
